489ae844f2f3ebc1bf7420be0cb7bda66e6da677
abhishekbhajantri <57219588+abhishekbhajantri@users.noreply.github.com>
2020-08-09 20:59:39 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+$hostname  = 'localhost';
+$username = 'id13436400_ac';
+$password='ql\C{?YaOWa^49[N';
+$dbname = 'id13436400_db2';
+$connect =  mysqli_connect($hostname , $username , $password ,$dbname) or die("Error Connecting");
+?>
